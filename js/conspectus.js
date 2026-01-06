3e7ba1bacdff667b32da035a2cc5f28fcb576194
Vadaska <159787695+Vadaska@users.noreply.github.com>
2026-01-06 17:02:57 +0200

    Update conspectus.js

--- js/conspectus.js
+++ js/conspectus.js
@@ -1,11 +1,29 @@
-const book = {
-  title: 'The Last Kingdom',
-  author: 'Bernard Cornwell',
-  genres: ['historical prose', 'adventure'],
-  rating: 8.38,
-};
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   genres: ['historical prose', 'adventure'],
+//   rating: 8.38,
+// };
 
-for (const key in book) {
-  console.log(key); // Ключ
-  console.log(book[key]); // Значення властивості з таким ключем
-}
+// for (const key in book) {
+//   console.log(key); // Ключ
+//   console.log(book[key]); // Значення властивості з таким ключем
+// }
+// const book = {
+//   author: 'Bernard Cornwell',
+//   genres: ['historical prose', 'adventure'],
+//   rating: 8.38,
+// };
+// const keys = Object.keys(book);
+
+// for (const key of keys) {
+//   console.log(key); // Ключ
+//   console.log(book[key]); // Значення властивості
+// }
+// !!!next
+// function countProps(object) {
+//   let propCount = Object.keys(object);
+
+//    return propCount.length;
+// }
+// !!!next
